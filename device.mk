b3d4b00c39a8243e8ab5c993fb60ef705318fb21
Lunarixus <Nathanb644@hotmail.co.uk>
2020-11-25 17:40:53 +0530

    davinci: Remove packages at build time

--- device.mk
+++ device.mk
@@ -333,6 +333,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
