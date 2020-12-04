4c19e57ea22070463927e6b952c6f3929b85ecab
AdarshGrewal <adarshgrewal@gmail.com>
2020-12-04 13:39:20 +0530

    davinci: add prebuilt GcamGo
    source https://www.celsoazevedo.com/files/android/google-camera/f/changelog1111/

--- device.mk
+++ device.mk
@@ -114,7 +114,7 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor
 
 PRODUCT_PACKAGES += \
-    Snap
+    GCamGo
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
