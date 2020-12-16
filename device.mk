a5774cff58f036bccb2986814bb91fb266d9a821
AdarshGrewal <adarshgrewal@gmail.com>
2020-12-16 15:03:10 +0530

    davinci: build telephony from QTI Components

--- device.mk
+++ device.mk
@@ -300,12 +300,14 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/permissions/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
+# QTI Components
+TARGET_COMMON_QTI_COMPONENTS := \
+    telephony
+
 # RIL
 PRODUCT_PACKAGES += \
-    libjson \
     libril \
-    librilutils \
-    librmnetctl
+    librilutils
 
 # Recovery
 PRODUCT_PACKAGES += \
